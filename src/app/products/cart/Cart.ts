7539c0c7bc2dc55d5d3b689f06cbd4b9719e9b97
Gorav <goravjindal86@gmail.com>
2024-09-04 21:23:09 +0530

    mcart

---
+++ src/app/products/cart/Cart.ts
@@ -0,0 +1,13 @@
+import { ProductListComponent } from "../product-list/product-list.component";
+// Cart Model
+export class Cart {
+    public orderId: string='';
+    public productId: number=0;
+    public userId: string='';
+    public productName: string='';
+    public quantity: number=0;
+    public dateOfPurchase: string='';
+    public price: number=0;
+    public totalPrice: number=0;
+}
+
